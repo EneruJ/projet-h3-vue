29d3fd9c5c46d28752ead9192c10fbceaf5666e2
Wassime Mezzache <62664268+EneruJ@users.noreply.github.com>
2022-12-11 19:58:31 +0100

    Modif posts

--- src/components/ShowPosts.vue
+++ src/components/ShowPosts.vue
@@ -1,64 +1,87 @@
 <template>
-  <div>
+<div>
     <BlogVue />
   </div>
-    <div>
-      <div v-for="post in posts" :key="post.id">
-        <p>Posté par {{ post.user }}</p>
-        <p>{{ post.content }}</p>
-        <img :src="post.imageUrl" />
-      </div>
+  <div>
+    <div v-for="post in posts" :key="post.id" class="post">
+      <h3>{{ post.user }}</h3>
+      <p>{{ post.content }}</p>
+      <img :src="post.imageUrl" />
+      <p>Likes : {{ post.like }} <button @click="addLike(post.id)">J'aime</button></p>
+      <form @submit.prevent="addComment(post.id)">
+        <label>Ajouter un commentaire :</label>
+        <input v-model="post.newComment" />
+        <button type="submit">Envoyer</button>
+      </form>
+      <ul v-if="post.comments">
+        <li v-for="comment in post.comments" :key="comment.id">
+          <p>Commentaire par {{ comment.user }}</p>
+          <p>{{ comment.content }}</p>
+        </li>
+      </ul>
     </div>
-  </template>
-<style>
-.post {
-  margin-bottom: 20px;
-  padding: 20px;
-  border: 10px solid #000;
-  border-radius: 5px;
-}
-.post p {
-    margin: 0;
-    text-align: center;
-  }
-img {
-  max-width: 100%;
-}
-</style>
-  <script>
-  import BlogVue from "./Blog.vue";
-  import firebase from "firebase/compat/app";
-  import 'firebase/compat/database';
+  </div>
+</template>
 
-  
-  export default {
-    name: 'PostList',
-    data() {
-      return {
-        posts: [],
-      };
-    },
-    components: {
+<script>
+// Importer les modules Vue et Firebase
+import firebase from "firebase/compat/app";
+import 'firebase/compat/database';
+import BlogVue from "./Blog.vue";
+
+export default {
+  data() {
+    return {
+      posts: [],
+      user: firebase.auth().currentUser,
+    };
+  },
+  components: {
       BlogVue,
     },
-    created() {
-      const db = firebase.database();
-      const postsRef = db.ref('posts');
-  
-      postsRef.on('value', snapshot => {
-        // Récupération des données des posts
-        const posts = snapshot.val();
-  
-        // Mise à jour de la liste des posts dans le composant
-        this.posts = Object.keys(posts).map(key => ({
-          id: key,
-          content: posts[key].content,
-          imageUrl: posts[key].imageUrl,
-          user: posts[key].user,
-        }));
-      });
+  created() {
+    const db = firebase.database();
+    // Récupérer les posts de la base de données lorsque le composant est créé
+    db.ref('posts').on('value', snapshot => {
+      this.posts = snapshot.val();
+    });
+  },
+  methods: {
+    // Méthode pour ajouter un like à un post
+    addLike(postId) {
+      // Récupérer le nombre de likes actuels
+      let likes = posts.find(post => post.id === postId).likes;
+
+      // Incrémenter le nombre de likes
+      likes++;
+
+      // Mettre à jour la base de données
+      db.ref(`posts/${postId}`).update({ likes });
     },
-  };
-  </script>
-  
-  
+    // Méthode pour ajouter un commentaire à un post
+    addComment(postId) {
+      // Récupérer le contenu du commentaire
+      const db = firebase.database();
+      let user = firebase.auth().currentUser;
+      let comment = this.posts.find((post) => post.id === postId).newComment || "";
+
+      // Ajouter le commentaire à la liste des commentaires du post
+      db.ref(`posts/${postId}/comments`).push({ content: comment, user: user.email });
+
+      // Réinitialiser le champ de saisie du commentaire
+      this.posts.find((post) => post.id === postId).newComment = '';
+    }
+  }
+};
+</script>
+<style>
+.post {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 500px;
+  margin: auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}</style>
